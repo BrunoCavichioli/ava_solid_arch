b4a855dc5e278d76def9c1c0f99de483a932c733
Bruno Cavichioli <brunocavichioli2019@gmail.com>
2026-05-13 00:47:37 -0300

    Task 5: Busca e Remoção de Pet por ID (getPetById e removePetById)

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -105,12 +105,60 @@ module.exports = class PetController {
             return
         }
         static async getPetById(req, res) {
-            res.status(200).json({ message: 'em construção' })
-            return
+            const id = req.params.id
+
+            if (!mongoose.Types.ObjectId.isValid(id)) {
+                res.status(422).json({ message: 'ID inválido!' })
+                return
+            }
+            
+            try {
+                const pet = await Pet.findById(id)
+                if (!pet) {
+                    res.status(404).json({ message: 'Pet não encontrado!' })
+                    return
+                }
+                res.status(200).json({ success: true, data: pet })
+            
+            } catch (error) {
+                res.status(503).json({ message: error })
+            }
+
+            const pet = await Pet.findById(id)
+
+            if (!pet) {
+                res.status(404).json({ message: 'Pet não encontrado' })
+                return
+            }
+
+            res.status(200).json({data: pet })
         }
         static async removePetById(req, res) {
-            res.status(200).json({ message: 'em construção' })
-            return
+            const id = req.params.id
+
+            if (!mongoose.Types.ObjectId.isValid(id)) {
+                res.status(422).json({ message: 'O id do pet é obrigatório!' })
+                return
+            }
+
+            const pet = await Pet.findById(id)
+
+            if (!pet) {
+                res.status(404).json({ message: 'Pet não encontrado!' })
+                return
+            }
+            
+            const token = getToken(req)
+            const user = await getUserByToken(token)
+
+            if (pet.user._id.toString() !== user._id.toString()) {
+                res.status(403).json({ message: 'apenas o dono pode remover o pet!' })
+                return
+            }
+
+            await Pet.findByIdAndDelete(id)
+            
+            res.status(200).json({ message: 'Pet removido com sucesso!', data: pet })
         }
         static async updatePet(req, res) {
             res.status(200).json({ message: 'em construção' })
